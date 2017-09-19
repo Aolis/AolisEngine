3aea1676a67409b98cd4f405dc5516628d0d59be
AntumDeluge <antumdeluge@gmail.com>
2017-09-18 18:17:33 -0700

    Add function for converting integer to string

---
+++ include/functions.h
@@ -0,0 +1,11 @@
+#ifndef AOLIS_FUNCTIONS_H
+#define AOLIS_FUNCTIONS_H
+
+#include <sstream>
+#include <string>
+
+
+extern const std::string toString(const int value);
+
+
+#endif /* AOLIS_FUNCTIONS_H */
---
+++ src/functions.cpp
@@ -0,0 +1,8 @@
+#include "functions.h"
+using namespace std;
+
+const string toString(const int value) {
+	ostringstream ss;
+	ss << value;
+	return ss.str();
+}
